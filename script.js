65f8ef89face5ce14019b0aad33a8c03a5c50218
Pieter Provoost <pieterprovoost@gmail.com>
2016-04-06 10:07:16 +0200

    added occurrence

--- script.js
+++ script.js
@@ -13,66 +13,114 @@ var parse = function(csv) {
 	return result;
 }
 
-var append = function(tree, node) {
-	if (tree.id == node.parentEventID) {
-		if (!tree.nodes) {
-			tree.nodes = [];
-		}
-		tree.nodes.push(createnode(node));
-		return true;
-	} else if (tree.nodes) {
-		for (n in tree.nodes) {
-			if (append(tree.nodes[n], node)) {
-				return true;
+var append = function(tree, node, occurrence) {
+	if (occurrence) {
+		if (tree.id == node.id) {
+			if (!tree.nodes) {
+				tree.nodes = [];
+			}
+			tree.nodes.push(createnode(node, occurrence));
+			return true;
+		} else if (tree.nodes) {
+			for (n in tree.nodes) {
+				if (append(tree.nodes[n], node, occurrence)) {
+					return true;
+				}
 			}
 		}
+		return false;
+	} else {
+		if (tree.id == node.parentEventID) {
+			if (!tree.nodes) {
+				tree.nodes = [];
+			}
+			tree.nodes.push(createnode(node, occurrence));
+			return true;
+		} else if (tree.nodes) {
+			for (n in tree.nodes) {
+				if (append(tree.nodes[n], node, occurrence)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
-	return false;
 };
 
-var createnode = function(data) {
-	var text = data.eventID;
-	if (data.generated) {
-		text = text + " <span class=\"label label-danger\">Generated</span>";
+var createnode = function(data, occurrence) {
+	if (occurrence) {
+		var text = data.occurrenceID;
+		if (data.scientificName && data.scientificName != "") {
+			text = text + " <span class=\"label label-warning\">" + data.scientificName + "</span>";
+		}
+		if (data.eventDate && data.eventDate != "") {
+			text = text + " <span class=\"label label-field\">eventDate: " + data.eventDate + "</span>";
+		}
+		if (data.decimalLongitude && data.decimalLongitude != "") {
+			text = text + " <span class=\"label label-field\">decimalLongitude: " + data.decimalLongitude + "</span>";
+		}
+		if (data.decimalLatitude && data.decimalLatitude != "") {
+			text = text + " <span class=\"label label-field\">decimalLatitude: " + data.decimalLatitude + "</span>";
+		}
+		if (data.minimumDepthInMeters && data.minimumDepthInMeters != "") {
+			text = text + " <span class=\"label label-field\">minimumDepthInMeters: " + data.minimumDepthInMeters + "</span>";
+		}
+		if (data.maximumDepthInMeters && data.maximumDepthInMeters != "") {
+			text = text + " <span class=\"label label-field\">maximumDepthInMeters: " + data.maximumDepthInMeters + "</span>";
+		}
+		var node = {
+			text: text,
+			id: data.occurrenceID
+		};
+		return node;
+	} else {
+		var text = data.eventID;
+		if (data.generated) {
+			text = text + " <span class=\"label label-danger\">Generated</span>";
+		}
+		if (data.eventDate && data.eventDate != "") {
+			text = text + " <span class=\"label label-field\">eventDate: " + data.eventDate + "</span>";
+		}
+		if (data.decimalLongitude && data.decimalLongitude != "") {
+			text = text + " <span class=\"label label-field\">decimalLongitude: " + data.decimalLongitude + "</span>";
+		}
+		if (data.decimalLatitude && data.decimalLatitude != "") {
+			text = text + " <span class=\"label label-field\">decimalLatitude: " + data.decimalLatitude + "</span>";
+		}
+		if (data.minimumDepthInMeters && data.minimumDepthInMeters != "") {
+			text = text + " <span class=\"label label-field\">minimumDepthInMeters: " + data.minimumDepthInMeters + "</span>";
+		}
+		if (data.maximumDepthInMeters && data.maximumDepthInMeters != "") {
+			text = text + " <span class=\"label label-field\">maximumDepthInMeters: " + data.maximumDepthInMeters + "</span>";
+		}
+		var node = {
+			text: text,
+			id: data.eventID
+		};
+		return node;
 	}
-	if (data.eventDate && data.eventDate != "") {
-		text = text + " <span class=\"label label-field\">eventDate: " + data.eventDate + "</span>";
-	}
-	if (data.decimalLongitude && data.decimalLongitude != "") {
-		text = text + " <span class=\"label label-field\">decimalLongitude: " + data.decimalLongitude + "</span>";
-	}
-	if (data.decimalLatitude && data.decimalLatitude != "") {
-		text = text + " <span class=\"label label-field\">decimalLatitude: " + data.decimalLatitude + "</span>";
-	}
-	if (data.minimumDepthInMeters && data.minimumDepthInMeters != "") {
-		text = text + " <span class=\"label label-field\">minimumDepthInMeters: " + data.minimumDepthInMeters + "</span>";
-	}
-	if (data.maximumDepthInMeters && data.maximumDepthInMeters != "") {
-		text = text + " <span class=\"label label-field\">maximumDepthInMeters: " + data.maximumDepthInMeters + "</span>";
-	}
-	var node = {
-		text: text,
-		id: data.eventID
-	};
-	return node;
 };
 
-var convert = function() {
+var tree = [];
+var errors = [];
+
+var process = function() {
 	$("#working").show();
 	$("#messages").html("");
 	$("#report").hide();
 	$("#wrapper").hide();
-	setTimeout(convert2);
+	setTimeout(function() {
+		tree = [];
+		errors = [];
+		process_event();
+		process_occurrence();
+		draw();
+	});
 };
 
-var convert2 = function() {
+var process_event = function() {
 
-	var errors = [];
-	var rootevents = 0;
-	var totalevents = 0;
-	var tree = [];
-
-	// parse inout
+	// parse input
 
 	var input = $("#event").val();
 	var data = parse(input);
@@ -109,11 +157,9 @@ var convert2 = function() {
 		}
 	}
 
-	// process nodes
+	// process lines
 
 	data.reverse();
-
-	totalevents = data.length;
 
 	while (data.length > 0) {
 
@@ -125,7 +171,6 @@ var convert2 = function() {
 
 				// no parentEventID present
 
-				rootevents++;
 				tree.push(createnode(data[i]));
 				data.splice(i, 1);
 
@@ -149,8 +194,33 @@ var convert2 = function() {
 		}
 
 	}
+};
 
-	// create tree
+var process_occurrence = function() {
+
+	// parse input
+
+	var input = $("#occurrence").val();
+	var data = parse(input);
+
+	// process lines
+
+	for (var i = 0; i < data.length; i++) {
+		var found = false;
+		for (t in tree) {
+			if (append(tree[t], data[i], true)) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			errors.push("No event " + data[i].id + " found for occurrence " + data[i].occurrenceID);
+		}
+	}
+
+};
+
+var draw = function() {
 
 	$("#wrapper").show();
 
@@ -160,14 +230,32 @@ var convert2 = function() {
 		showBorder: false
 	});
 
-	var messages = "";
-	for (e in errors) {
-		messages = messages + errors[e] + "\n";
+	if (errors.length > 0) {
+		var messages = "";
+		for (e in errors) {
+			messages = messages + errors[e] + "\n";
+		}
+		$("#messages").html(messages);
+		$("#report").show();
 	}
-
-	$("#messages").html(messages);
-	$("#report").show();
 
 	$("#working").hide();
 
 };
+
+$(document).ready(function() {
+	$.ajax({
+		url: "event.txt",
+		dataType: "text",
+		success: function(data) {
+			$("#event").text(data);
+		}
+	});
+	$.ajax({
+		url: "occurrence.txt",
+		dataType: "text",
+		success: function(data) {
+			$("#occurrence").text(data);
+		}
+	});
+}); 
